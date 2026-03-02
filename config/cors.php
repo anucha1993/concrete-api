8b6c4c955c116683232c19465d1eca5285f608d0
anucha1993 <108377127+anucha1993@users.noreply.github.com>
2026-03-02 15:00:26 +0700

    asdsad

--- config/cors.php
+++ config/cors.php
@@ -12,9 +12,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
+    'allowed_origins' => array_filter([
         env('FRONTEND_URL', 'http://localhost:3000'),
-    ],
+        env('FRONTEND_URL_PRODUCTION'),
+    ]),
 
     'allowed_origins_patterns' => [],
 
